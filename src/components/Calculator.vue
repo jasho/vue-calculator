66ec9fbd5b129bd7d1142322d9b8fa6a7e312edb
Kyle Butler <kylbutlr@gmail.com>
2019-04-22 16:11:38 -0700

    calculator methods

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,35 +1,95 @@
 <template>
-  <div class='calculator'>
-    <div class='display'>0</div>
-    <div class='button'>C</div>
-    <div class='button'>+/-</div>
-    <div class='button'>%</div>
-    <div class='button operator'>÷</div>
-    <div class='button'>7</div>
-    <div class='button'>8</div>
-    <div class='button'>9</div>
-    <div class='button operator'>x</div>
-    <div class='button'>4</div>
-    <div class='button'>5</div>
-    <div class='button'>6</div>
-    <div class='button operator'>-</div>
-    <div class='button'>1</div>
-    <div class='button'>2</div>
-    <div class='button'>3</div>
-    <div class='button operator'>+</div>
-    <div class='button zero'>0</div>
-    <div class='button'>.</div>
-    <div class='button operator'>=</div>
+  <div class="calculator">
+    <div class="display">{{display}}</div>
+    <div @click="clear" class="button">C</div>
+    <div @click="sign" class="button">+/-</div>
+    <div @click="percent" class="button">%</div>
+    <div @click="divide" class="button operator">÷</div>
+    <div @click="append(7)" class="button">7</div>
+    <div @click="append(9)" class="button">9</div>
+    <div @click="append(8)" class="button">8</div>
+    <div @click="multiply" class="button operator">x</div>
+    <div @click="append(4)" class="button">4</div>
+    <div @click="append(5)" class="button">5</div>
+    <div @click="append(6)" class="button">6</div>
+    <div @click="subtract" class="button operator">-</div>
+    <div @click="append(1)" class="button">1</div>
+    <div @click="append(2)" class="button">2</div>
+    <div @click="append(3)" class="button">3</div>
+    <div @click="add" class="button operator">+</div>
+    <div @click="append(0)" class="button zero">0</div>
+    <div @click="decimal" class="button">.</div>
+    <div @click="equal" class="button operator">=</div>
   </div>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      previous: null,
+      display: 0,
+      operator: null,
+      operatorClicked: false
+    };
+  },
+  methods: {
+    clear() {
+      this.display = 0;
+    },
+    sign() {
+      this.display =
+        this.display < 0
+          ? (this.display = this.display - this.display * 2)
+          : (this.display = this.display - this.display * 2);
+    },
+    percent() {
+      this.display = this.display / 100;
+    },
+    append(number) {
+      if (this.operatorClicked === true) {
+        this.display = "";
+        this.operatorClicked = false;
+      }
+      this.display =
+        this.display === 0
+          ? (this.display = number)
+          : "" + this.display + number;
+    },
+    decimal() {
+      if (this.display.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.previous = this.display;
+      this.operatorClicked = true;
+    },
+    multiply() {
+      this.operator = (a, b) => a * b;
+      this.previous = this.display;
+      this.operatorClicked = true;
+    },
+    subtract() {
+      this.operator = (a, b) => a - b;
+      this.previous = this.display;
+      this.operatorClicked = true;
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.previous = this.display;
+      this.operatorClicked = true;
+    },
+    equal() {
+      this.display = this.operator(this.previous, this.display);
+      this.previous = null;
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 .calculator {
   margin: auto;
   width: 60vw;
@@ -63,5 +123,4 @@ export default {
 .operator {
   background-color: #acce33;
 }
-
 </style>
